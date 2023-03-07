0743cdad2aca6191fd261ca082bbcc141ad6cb0a
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-07 11:46:13 -0300

    Add files via upload

---
+++ upcar/src/main/java/hiag0liveira/github/io/upcar/domain/entity/Carro.java
@@ -0,0 +1,167 @@
+package hiag0liveira.github.io.upcar.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table( name = "carro" )
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+
+    @Column(name = "marca", length = 100)
+    private String marca;
+
+    @Column(name = "modelo", length = 100)
+    private String modelo;
+
+    @Column(name = "cor", length = 100)
+    private String cor;
+
+    @Column(name = "placa", length = 8)
+    private String placa;
+
+    @Column(name = "km_rodado", length = 100)
+    private String km_rodado;
+
+    @Column(name = "descricao", length = 300)
+    private String descricao;
+
+    @Column(name = "anoFabricacao")
+    private int anoFabricacao;
+
+    @Column(name = "anoModelo")
+    private int anoModelo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipoCombustivel")
+    private TipoCombustivelEnum tipoCombustivelEnum;
+
+    public Carro(String nome, String marca, String modelo, String cor, String placa, String km_rodado, String descricao,
+            int anoFabricacao, int anoModelo, TipoCombustivelEnum tipoCombustivelEnum) {
+        this.nome = nome;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.placa = placa;
+        this.km_rodado = km_rodado;
+        this.descricao = descricao;
+        this.anoFabricacao = anoFabricacao;
+        this.anoModelo = anoModelo;
+        this.tipoCombustivelEnum = tipoCombustivelEnum;
+    }
+
+    public Carro() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getKm_rodado() {
+        return km_rodado;
+    }
+
+    public void setKm_rodado(String km_rodado) {
+        this.km_rodado = km_rodado;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public int getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(int anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public TipoCombustivelEnum getTipoCombustivelEnum() {
+        return tipoCombustivelEnum;
+    }
+
+    public void setTipoCombustivelEnum(TipoCombustivelEnum tipoCombustivelEnum) {
+        this.tipoCombustivelEnum = tipoCombustivelEnum;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", cor='" + cor + '\'' +
+                ", placa='" + placa + '\'' +
+                ", km_rodado='" + km_rodado + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", anoFabricacao=" + anoFabricacao +
+                ", anoModelo=" + anoModelo +
+                ", tipoCombustivelEnum=" + tipoCombustivelEnum +
+                '}';
+    }
+}
---
+++ upcar/src/main/java/hiag0liveira/github/io/upcar/domain/entity/Endereco.java
@@ -0,0 +1,127 @@
+package hiag0liveira.github.io.upcar.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table( name = "endereco" )
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "rua", length = 50)
+    private String rua;
+
+    @Column(name = "numero",length = 10)
+    private String numero;
+
+    @Column(name = "cep", length = 8)
+    private String cep;
+
+    @Column(name = "complemento", length = 50)
+    private String complemento;
+
+    @Column(name = "cidade", length = 50)
+    private String cidade;
+
+    @Column(name = "bairro", length = 50)
+    private String bairro;
+
+    @Column(name = "estado", length = 50)
+    private String estado;
+    
+    public Endereco(String rua, String numero, String cep, String complemento, String cidade, String bairro,
+            String estado) {
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+        this.complemento = complemento;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.estado = estado;
+    }
+
+    public Endereco() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "id=" + id +
+                ", rua='" + rua + '\'' +
+                ", numero='" + numero + '\'' +
+                ", cep='" + cep + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
