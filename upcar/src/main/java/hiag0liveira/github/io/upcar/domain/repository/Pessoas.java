5b6800653763a046ccf9554548aa05265a7b26af
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-07 10:57:58 -0300

    Delete upcar directory

--- upcar/src/main/java/hiag0liveira/github/io/upcar/domain/repository/Pessoas.java
+++
@@ -1,7 +0,0 @@
-package hiag0liveira.github.io.upcar.domain.repository;
-
-import hiag0liveira.github.io.upcar.domain.entity.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Pessoas  extends JpaRepository<Pessoa,Integer> {
-}
