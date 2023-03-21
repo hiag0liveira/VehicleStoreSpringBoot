9745241354be00b9ee04ec1864313875f04dc3d0
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-20 21:15:54 -0300

    Add files via upload

--- upcar/src/main/java/hiag0liveira/github/io/upcar/rest/controller/apirest/ClienteController.java
+++ upcar/src/main/java/hiag0liveira/github/io/upcar/rest/controller/apirest/ClienteController.java
@@ -2,76 +2,53 @@ package hiag0liveira.github.io.upcar.rest.controller.apirest;
 
 import hiag0liveira.github.io.upcar.domain.entity.Cliente;
 import hiag0liveira.github.io.upcar.domain.repository.Clientes;
+import hiag0liveira.github.io.upcar.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/apirest/clientes")
 public class ClienteController {
 
-    private Clientes clientes;
+    @Autowired
+    private ClienteService service;
 
-    public ClienteController( Clientes clientes ) {
-        this.clientes = clientes;
-    }
 
     @GetMapping("{id}")
-    public Cliente getClienteById(@PathVariable Integer id ){
-        return clientes
-                .findById(id)
-                .orElseThrow(() ->
-                        new ResponseStatusException(HttpStatus.NOT_FOUND,
-                                "Cliente não encontrado"));
-    }
+    public ResponseEntity OneClienteById(@PathVariable Integer id ){
+        return ResponseEntity.ok(service.getClienteById(id));
+        }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente save( @RequestBody Cliente cliente ){
-        return clientes.save(cliente);
+    public ResponseEntity saveNewCliente( @RequestBody Cliente cliente ){
+       service.saveOneNewCliente(cliente);
+       return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete( @PathVariable Integer id ){
-        clientes.findById(id)
-                .map( cliente -> {
-                    clientes.delete(cliente );
-                    return cliente;
-                })
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Cliente não encontrado") );
-
+    public ResponseEntity OneDeleteClienteByID( @PathVariable Integer id ){
+        service.deleteClienteByID(id);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update( @PathVariable Integer id,
-                        @RequestBody Cliente cliente ){
-        clientes
-                .findById(id)
-                .map( clienteExistente -> {
-                    cliente.setId(clienteExistente.getId());
-                    clientes.save(cliente);
-                    return clienteExistente;
-                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Cliente não encontrado") );
+    public ResponseEntity updateClienteByID( @PathVariable Integer id, @RequestBody Cliente cliente ){
+        service.updateCliente(id , cliente);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public List<Cliente> find( Cliente filtro ){
-        ExampleMatcher matcher = ExampleMatcher
-                .matching()
-                .withIgnoreCase()
-                .withStringMatcher(
-                        ExampleMatcher.StringMatcher.CONTAINING );
-
-        Example example = Example.of(filtro, matcher);
-        return clientes.findAll(example);
+    public ResponseEntity findAllClientes(){
+        return ResponseEntity.ok(service.findAllClientes());
     }
-
 }
